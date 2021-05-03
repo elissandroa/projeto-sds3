79d47ce878cf47c7f155aafbeb7919f5822f0142
Elissandro <elissandroa@hotmail.com>
2021-05-03 10:17:53 -0300

    Project created

---
+++ backend/src/main/java/com/elissandro/projetosds3/ProjetoSds3Application.java
@@ -0,0 +1,13 @@
+package com.elissandro.projetosds3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSds3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSds3Application.class, args);
+	}
+
+}
